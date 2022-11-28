57c856ee27ed4b135034b6d87beb4a59f9525a88
David Shannon <dzshanno@yahoo.com>
2022-11-28 18:09:40 +0000

    use itemgetter to find minimum

--- 2022 Q6 - triangles V2.py
+++ 2022 Q6 - triangles V2.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 import triangles
+import operator
 
 # Create list of possible triangles
 allpossible = list(permutations(['1','2','3','4','5','6','7','8','9']))
@@ -9,24 +10,20 @@ for possible in allpossible:
     first = int(possible[0]+possible[1]+possible[2])
     second = int(possible[3]+possible[4]+possible[5])
     third = int(possible[6]+possible[7]+possible[8])
-    threesides.append([first,second,third]) 
+    threesides.append([first,second,third,0]) 
     
 #create a list of just those threesides that make legitimate tringles with non-zero area
-list_of_triangles = [test for test in  threesides if triangles.is_triangle(test)]
-
-# define the largest and smallest area found
-largest = [0,['','','']]
-smallest = [float('inf'),['','','']]
+list_of_triangles = [test for test in  threesides if triangles.is_triangle([test[0],test[1],test[2]])]
 
 #loop through all triangles
 for testcase in list_of_triangles:
-    
-    testarea = triangles.area(testcase)
-        
-    if testarea > largest[0]:
-        largest = testarea, testcase
-    if testarea < smallest[0]:
-        smallest = testarea, testcase
-        
-print(largest)
-print(smallest)
+    sides = [testcase[0],testcase[1],testcase[2]]
+    testarea = triangles.area(sides)
+    testcase[3] = testarea    
+
+smallest = min(list_of_triangles, key=operator.itemgetter(3))
+largest = max(list_of_triangles, key=operator.itemgetter(3))
+
+# print output  
+print(f"largest {largest}")
+print(f"smallest {smallest}")
