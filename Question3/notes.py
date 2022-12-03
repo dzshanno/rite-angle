4b20ebf593a14509e2976d655b7d98c1e6a005e3
David Shannon <dzshanno@yahoo.com>
2022-12-03 12:44:06 +0000

    Create notes.py

---
+++ Question3/notes.py
@@ -0,0 +1,12 @@
+The missing values a,b c,d e,f   and  are the digits 1,2 3,4 5   and 6 in some order.
+
+The numbers
+
+ab,c5d, e4f
+
+
+are in arithmetic progression.
+
+What is the maximum possible product for the three numbers? 
+
+To get your final answer, multiply this by 3.8 x 10-6  and take the integer part. 
